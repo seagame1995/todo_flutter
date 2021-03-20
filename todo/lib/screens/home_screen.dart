f4484510af785cc19592fcab8ab65bbaf1fa8c86
seagame1995 <49188594+seagame1995@users.noreply.github.com>
2021-03-20 14:30:42 +0700

    todo init

---
+++ todo/lib/screens/home_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> todos = [];
+  TextEditingController input = new TextEditingController();
+
+  void handleAddTolist() {
+    setState(() {
+      todos.add(input.text);
+      input.text = "";
+    });
+  }
+
+  void handleRemoveTodoFromList(String item) {
+    setState(() {
+      todos.remove(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo list"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Form(
+              child: TextFormField(
+                controller: input,
+                decoration: InputDecoration(
+                  hintText: "รายการที่ต้องทำ",
+                  border: OutlineInputBorder(),
+                  suffixIcon: Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 12.0),
+                    child: IconButton(
+                      onPressed: () {
+                        handleAddTolist();
+                        },
+                      icon: Icon(
+                        Icons.list,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView(
+                children: todos
+                    .map(
+                      (item) => Card(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("$item"),
+                                IconButton(
+                                  onPressed: () {
+                                    handleRemoveTodoFromList(item);
+                                  },
+                                  icon: Icon(
+                                    Icons.delete,
+                                    color: Colors.red,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
